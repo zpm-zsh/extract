6186bd61c53d8230f9e71f18ef026259157688db
Grigorii Horos <horosgrisa@gmail.com>
2021-12-13 22:24:57 +0200

    Initial commit

---
+++ extract.plugin.zsh
@@ -0,0 +1,10 @@
+# Standarized $0 handling, following:
+# https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+
+autoload -Uz extract
+
+alias x=extract
+
